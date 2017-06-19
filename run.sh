0bc1d838eccdca45aca5e180fd904c5bfbb23682
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-06-19 14:54:56 -0300

    change pidbind exection to /opt/pidbind

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-./pidbind.sh &
+/opt/pidbind.sh &
+
+DOCKER=`docker ps -a | wc -l`
+if [[ $DOCKER > 1 ]];
+    docker rm -f $(docker ps -a)
+fi
+
 
 docker run -it --rm --network bridge --cap-add NET_ADMIN --name=wifi-container futebolufmg/ubuntu-wifi
 
