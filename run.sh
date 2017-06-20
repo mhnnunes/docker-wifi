cd09db6be40b2aaf0a04cb23eeea0a8b85bdb8fb
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-06-20 15:44:36 -0300

    Change to docker exec in order to keep files intact

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 DOCKER=`docker ps -f name=wifi-container | wc -l`
-if [ $DOCKER > 1 ]
+
+if (( $DOCKER == 1 ))
 then
-    # docker rm -f $(docker ps -a | tail -n +2 |awk '{print $1}' ) &> /dev/null
-    docker attach wifi-container
-else
     /opt/docker-wifi/pidbind.sh &
-    docker run -it --network bridge --cap-add NET_ADMIN --name=wifi-container futebolufmg/ubuntu-wifi
+    docker run -dit --network bridge --cap-add NET_ADMIN --name=wifi-container futebolufmg/ubuntu-wifi
 fi
 
-
+docker exec -ti wifi-container bash
