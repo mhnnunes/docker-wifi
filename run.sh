d09b68eba075539d5270a12d6dc33acc1ae5e6a1
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-06-20 15:25:22 -0300

    Change run.sh to attach to existing container

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DOCKER=`docker ps -a | wc -l`
+DOCKER=`docker ps -f name=wifi-container`
 if [ $DOCKER > 1 ]
 then
-    docker rm -f $(docker ps -a | tail -n +2 |awk '{print $1}' ) &> /dev/null
+    # docker rm -f $(docker ps -a | tail -n +2 |awk '{print $1}' ) &> /dev/null
+    docker attach wifi-container
+else
+    /opt/docker-wifi/pidbind.sh &
+    docker run -it --network bridge --cap-add NET_ADMIN --name=wifi-container futebolufmg/ubuntu-wifi
 fi
-
-/opt/docker-wifi/pidbind.sh &
-
-docker run -it --rm --network bridge --cap-add NET_ADMIN --name=wifi-container futebolufmg/ubuntu-wifi
 
 
