98bf6ccfdfbc7b4bed343bb35ac6bc0b34189e06
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-07-07 14:02:26 -0300

    Add rfkill to container

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ DOCKER=`docker ps -f name=wifi-container | wc -l`
 if (( $DOCKER == 1 ))
 then
     /opt/docker-wifi/pidbind.sh &
-    docker run -dit --network bridge --cap-add=NET_ADMIN --cap-add=SYS_MODULE --name=wifi-container futebolufmg/ubuntu14-wifi
+    docker run -dit --device /dev/rfkill:/dev/rfkill --network bridge --cap-add=NET_ADMIN --cap-add=SYS_MODULE --name=wifi-container futebolufmg/ubuntu14-wifi
 fi
 
 docker exec -ti wifi-container bash
