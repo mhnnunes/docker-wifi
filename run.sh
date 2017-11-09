d7a0ad064f3e94c688204b7133e48089543ff29f
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-11-09 14:16:52 -0200

    Substitute image name for script parmeter

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [image_type (options: ethanol | ubuntu16 | \
+ubuntu14]"
+    exit 0
+fi
+
 DOCKER=`docker ps -f name=wifi-container | wc -l`
 
 if (( $DOCKER == 1 ))
 then
     /opt/docker-wifi/pidbind.sh &
-    docker run -dit --device /dev/rfkill:/dev/rfkill --network bridge --cap-add=NET_ADMIN --cap-add=SYS_MODULE --name=wifi-container image_type
+    docker run -dit --device /dev/rfkill:/dev/rfkill --network bridge --cap-add=NET_ADMIN --cap-add=SYS_MODULE --name=wifi-container $1
     #docker run -dit --device /dev/rfkill:/dev/rfkill --privileged --network bridge --name=wifi-container image_type
 fi
 
