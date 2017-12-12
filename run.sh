7b6e3c43f63752475d9e2524b3a2c1adf3ffc5a9
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2017-12-12 07:42:35 -0200

    Change order of pidbind in run.sh

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ DOCKER=`docker ps -f name=wifi-container | wc -l`
 
 if (( $DOCKER == 1 ))
 then
-    /opt/docker-wifi/pidbind.sh &
     docker run -dit --device /dev/rfkill:/dev/rfkill --privileged --network bridge --cap-add=NET_ADMIN --cap-add=SYS_MODULE --name=wifi-container $1 > /dev/null 2> /dev/null
     #docker run -dit --device /dev/rfkill:/dev/rfkill --privileged --network bridge --name=wifi-container image_type
+    /opt/docker-wifi/pidbind.sh &
 fi
 
 docker exec -ti wifi-container bash
